bebd9a6632d99a249f0c9cefe08d57a2cc2ea4ac
DarkDestry <tanki123@hotmail.com>
2025-04-18 02:44:04 +0800

    Implement pooling

--- Scroll View/Assets/Scripts/InventoryManager.cs
+++ Scroll View/Assets/Scripts/InventoryManager.cs
@@ -6,35 +6,68 @@ using UnityEngine.UI;
 public class InventoryManager : MonoBehaviour
 {
     [SerializeField] private RectTransform contentHolder;
+    [SerializeField] private RectTransform poolHolder;
     [SerializeField] private VerticalLayoutGroup verticalLayoutGroup;
     [SerializeField] private InventoryRow inventoryRow;
-    
+
     [SerializeField] private int numRows = 10;
-    public GameObject[] inventoryRows;
+    [SerializeField] private int numPoolRows = 10;
+    public GameObject[] inventoryRowPool;
+
+    public Vector2 RowSize { get; private set; }
 
     private void Start()
     {
-        inventoryRows = new GameObject[numRows];
+        inventoryRowPool = new GameObject[numPoolRows];
         PopulateInventoryDisplay();
     }
 
     private void PopulateInventoryDisplay()
     {
-        for (var i = 0; i < numRows; i++)
+        for (var i = 0; i < numPoolRows; i++)
         {
-            var row = Instantiate(inventoryRow, contentHolder);
-            row.Init(i);
-            inventoryRows[i] = row.gameObject;
+            var row = Instantiate(inventoryRow, poolHolder);
+            if (i == 0)
+            {
+                LayoutRebuilder.ForceRebuildLayoutImmediate(row.GetComponent<RectTransform>());
+                var rowLayout = row.GetComponent<HorizontalLayoutGroup>();
+                RowSize = new Vector2(rowLayout.preferredWidth, rowLayout.preferredHeight);
+            }
+
+            row.gameObject.SetActive(false);
+            inventoryRowPool[i] = row.gameObject;
         }
+
         SetContentHeight();
     }
 
     private void SetContentHeight()
     {
-        LayoutRebuilder.ForceRebuildLayoutImmediate(contentHolder);
-        var contentHeight = verticalLayoutGroup.preferredHeight;
-        var contentWidth = verticalLayoutGroup.preferredWidth;
+        var contentHeight = RowSize.y * numRows +
+                            verticalLayoutGroup.spacing * (numRows - 1) +
+                            verticalLayoutGroup.padding.top +
+                            verticalLayoutGroup.padding.bottom;
+        var contentWidth = RowSize.x;
+
         verticalLayoutGroup.enabled = false;
         contentHolder.sizeDelta = new Vector2(contentWidth, contentHeight);
+    }
+
+    public void ReturnToPool(GameObject go)
+    {
+        go.transform.SetParent(poolHolder);
+        go.SetActive(false);
+    }
+
+    public InventoryRow InitFromPool(int index)
+    {
+        var poolItem = poolHolder.GetChild(0);
+        poolItem.transform.SetParent(contentHolder);
+
+        var rowComponent = poolItem.GetComponent<InventoryRow>();
+        rowComponent.Init(index);
+
+        poolItem.gameObject.SetActive(true);
+        return rowComponent;
     }
 }
--- Scroll View/Assets/Scripts/OptimizeScroll.cs
+++ Scroll View/Assets/Scripts/OptimizeScroll.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,10 +9,12 @@ public class OptimizeScroll : MonoBehaviour
 {
     [SerializeField] private RectTransform viewPort;
     [SerializeField] private ScrollRect scrollRect;
+    [SerializeField] private VerticalLayoutGroup verticalLayoutGroup;
     
     [SerializeField] private InventoryManager inventoryManager;
 
     private RectTransform[] rowTrs; 
+    private Dictionary<int, InventoryRow> rowItems = new();
     
     private void OnEnable()
     {
@@ -23,34 +26,38 @@ public class OptimizeScroll : MonoBehaviour
     IEnumerator DelayCullRows()
     {
         yield return new WaitForEndOfFrame();
-        UpdateVisibleItems();
+        HandleScroll(Vector2.zero);
     }
     
-    private void HandleScroll(Vector2 value)
+    private void HandleScroll(Vector2 _)
     {
-        UpdateVisibleItems();
-    }
+        var currentPos = verticalLayoutGroup.transform.localPosition;
+        var height = viewPort.rect.height;
+        var rowHeight = verticalLayoutGroup.spacing + inventoryManager.RowSize.y; 
+        var startIndex = Mathf.FloorToInt(currentPos.y / rowHeight);
+        var endIndex = Mathf.CeilToInt((currentPos.y + height) / rowHeight);
 
-    private const int BOTTOM_LEFT = 0;
-    private const int TOP_LEFT = 1;
-    private const int TOP_RIGHT = 2;
-    private const int BOTTOM_RIGHT = 3;
-
-    // Outside of scope to avoid GC
-    Vector3[] rowCorners = new Vector3[4];
-    Vector3[] viewPortCorners = new Vector3[4];
-    private void UpdateVisibleItems()
-    {
-        // Implement your solution here
-        // Access the array of inventory rows as needed: inventoryManager.inventoryRows
-        viewPort.GetWorldCorners(viewPortCorners);
-        rowTrs ??= inventoryManager.inventoryRows.Select(go => go.GetComponent<RectTransform>()).ToArray();
-        
-        foreach (var rowTr in rowTrs)
+        var toRemoveIndices = new List<int>();
+        foreach (var index in rowItems.Keys)
         {
-            rowTr.GetWorldCorners(rowCorners);
-            rowTr.gameObject.SetActive(!(rowCorners[BOTTOM_LEFT].y > viewPortCorners[TOP_LEFT].y ||
-                           rowCorners[TOP_LEFT].y < viewPortCorners[BOTTOM_LEFT].y));
+            if (index >= startIndex && index <= endIndex) continue;
+            var row = rowItems[index];
+            toRemoveIndices.Add(index);
+            inventoryManager.ReturnToPool(row.gameObject);
         }
+
+        foreach (var index in toRemoveIndices) rowItems.Remove(index);
+
+        for (int i = startIndex; i <= endIndex; i++)
+        {
+            if (rowItems.ContainsKey(i)) continue;
+            var row = inventoryManager.InitFromPool(i);
+            var posX = verticalLayoutGroup.padding.left;
+            var posY = verticalLayoutGroup.padding.top + //Top Padding 
+                       rowHeight * i; // Previous Row heights and spacings
+            row.transform.localPosition = new Vector2(posX, -posY);
+            rowItems[i] = row;
+        }
+
     }
 }
